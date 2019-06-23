324d707f073f8877126852bf413a51eb7e864818
Admin <eaak2004@hotmail.com>
2019-06-23 22:33:42 +0100

    Add project files.

---
+++ WebApplication4/Scripts/Site.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $("#btnShow").mousedown(function () {
+        $("#Password").attr("type", "text");
+    });
+
+    $("#btnShow").on("mouseleave", function () {
+        $("#Password").attr("type", "password");
+    });
+});
